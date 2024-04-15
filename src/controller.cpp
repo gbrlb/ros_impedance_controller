a49d2ac6e0a3ca55ebf194694c6158dac8b4ab35
gbrlb <gbermudezar@gmail.com>
2024-04-15 20:06:58 -0300

    fix limit param

--- src/controller.cpp
+++ src/controller.cpp
@@ -530,7 +530,6 @@ namespace ros_impedance_controller
 
                 des_joint_efforts_sum_(i) = des_joint_efforts_(i) + des_joint_efforts_pids_(i);
 
-                double effort_limit = 5.0; //23.7 max
                 if (std::abs(des_joint_efforts_sum_(i)) > effort_limit)
                 {
                     des_joint_efforts_sum_(i) = (des_joint_efforts_sum_(i) / std::abs(des_joint_efforts_sum_(i))) * effort_limit;
